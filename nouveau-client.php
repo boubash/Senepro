18fce7d087a8a61d8301e32786dd0891ce851d61
booba <boubash4@gmail.com>
2013-09-04 20:05:42 +0100

    ajout du formulaire client

--- nouveau-client.php
+++ nouveau-client.php
@@ -1,12 +1,33 @@
 <?php include('header.php') ?>
+<div class="container">
+	<div class="row">
+		<div class="span9"><h1>Ajout Client</h1></div>
+		<div class="span3"><a href="client.php" class="btn pull-right" >Liste Clients</a></div>
+	
+	</div>
+	 
+	 
+	 
+	<form method="post" action="">
 
-	
-	<form>
-	
 		
-	
-	</form>
+					<input type="hidden" name="id_client">
+			     	<input type="text" name="nom" placeholder="Nom:">
+				
+				<input type="text" name="pnom" placeholder="Prenom:">
+			
+				<input type="text" name="adr" placeholder="Adresse:">
+				
+				<input type="text" name="tel" placeholder="telephone">
+				
+			    	
+								
+				<input type="submit" name="ok" value="Ajouter">   
 
+
+
+</form>
+</div>
 
 
 <?php include('footer.php') ?> 
