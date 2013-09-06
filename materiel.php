11804ede79a3e942185aa73f3a0ed39f9e34ddb2
booba <boubash4@gmail.com>
2013-09-06 17:49:03 +0100

    ajout table suppression dans materiel.php

--- materiel.php
+++ materiel.php
@@ -9,23 +9,40 @@
 	
 				<a href='nouveau-materiel.php' class='btn btn-success pull-right' >
 					<i class='icon-plus'></i>
+
 					Nouveau materiel 
 				</a>
 			</div>
 	</div>
 		<table class='table table-bordered'>
 			<tr>
+				<th>#</th>
 				<th>désignation</th>
 				<th>prix</th>
 				<th>actions</th>
 				
 			</tr>
 			
-			<?php // ajouter les lignes provenant de la base ?>
+			<?php 
+
+					include("conexion.php");
+							$req="select * from materiel ";
+							$exe=mysql_query($req);
+							    while($l= mysql_fetch_array($exe))
+							    {
+
+								echo"<tr>
+									<td>".$l[0]."</td>
+									<td>".$l[1]."</td>
+									<td>".$l[2]."</td>
+									<td><a href=\"suppression.php?matricule=".$l['matricule']."\" onclick=\"return(confirm('Etes-vous sûr de vouloir supprimer cette entrée?'));\" ><img src=\"b_drop.png\"></a></td> 
+					
+								</tr>";
+
+								 }
+					echo"</table>";
+			?>
 			
-		</table>
 	</div>
-
-
 
 <?php include('footer.php') ?> 
