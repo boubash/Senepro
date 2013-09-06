65871b4f3f23dcee61f705179f9a09cc31f34a75
booba <boubash4@gmail.com>
2013-09-06 23:03:30 +0100

    creation suppression.php

--- suppression.php
+++ suppression.php
@@ -4,11 +4,11 @@
   $id  = $_GET['matricule'] ;
   
   $sql = "DELETE FROM client WHERE id_client = ".$id ;
-  echo $sql ;	    
+    
 
   $exe = mysql_query($sql) ;
  
-  //affichage des résultats, pour savoir si la suppression a marchée:
+
   if($exe)
   {
     echo("La suppression à été correctement effectuée") ;
