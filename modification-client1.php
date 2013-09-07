1af9642e279ed3c0eee5e583964ec9767dbd154e
booba <boubash4@gmail.com>
2013-09-07 02:28:05 +0100

    suppression des accent

--- modification-client1.php
+++ modification-client1.php
@@ -2,42 +2,41 @@
 
  <?php
    include("conexion.php");
+  
+          $id=$_GET["matricule"] ;
 
-    $id=$_GET["matricule"] ;
+          $req="SELECT * FROM client WHERE id_client =".$id;
 
-     $req="SELECT * FROM client WHERE matricule =".$id;
+          $exe=mysql_query($req);
 
-      $exe=mysql_query($sql);
- 
-      if($l=mysql_fetch_array($exe))
-      {
-
+            if($l=mysql_fetch_array($exe))
+          {
   ?>
 
         <form method="POST" name="insertion" action="modification-client2.php" >
 
-          <input type="hidden" name="id_client" value="<?php echo $id_mclient ;?>">
+          <input type="hidden" name="id_client" value="<?php echo $id_client ;?>">
 
           <table class='table table-bordered'>
 
               <tr >
                 <td>Nom</td>
-                <td><input type="text" name="nom" value="<?php echo $l['nom'] ;?>"></td>
+                <td><input type="text" name="nom" value="<?php echo $l['1'] ;?>"></td>
               </tr>
 
               <tr >
                 <td>Prenom</td>
-                <td><input type="text" name="prenom" value="<?php echo $l['prenom'] ;?>"></td>
+                <td><input type="text" name="prenom" value="<?php echo $l['2'] ;?>"></td>
               </tr>
              
              <tr >
                 <td>Adresse</td>
-                <td><input type="text" name="adresse" value="<?php echo $l['adresse'] ;?>"></td>
+                <td><input type="text" name="adresse" value="<?php echo $l['3'] ;?>"></td>
               </tr>
 
               <tr >
                 <td>t&aecut;l&aecut;phone</td>
-                <td><input type="text" name="telephone" value="<?php echo $l['telephone'] ;?>"></td>
+                <td><input type="text" name="telephone" value="<?php echo $l['4'] ;?>"></td>
               </tr>
 
               <tr >
