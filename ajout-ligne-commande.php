aa820295e4ef3e43b3446ce1b45ad95154d79a44
booba <boubash4@gmail.com>
2013-10-03 18:44:16 +0000

    changement code php

---
+++ ajout-ligne-commande.php
@@ -0,0 +1,20 @@
+﻿<?php
+if(isset($_POST['ok']))
+
+{
+
+include("conexion.php");
+
+extract($_POST);
+
+
+$req="insert into  ligne_de_commande values('', $quantite,$materiel,$id_commande)";
+echo"$req";
+$exe=mysql_query($req);
+
+header("Location:detail-commande.php?id_commande=".$id_commande);
+
+}
+  
+?>
+
--- index.html.php
+++ index.html.php
@@ -1,5 +1,40 @@
 <?php include('header.php') ?>
 
+<?php require_once 'header.php';?>
 
+	<div class="container">
+		<div class="row">
+			<div class="span6" align="center"><h3>Commandes Clients</h3></div>
+		</div>
+		<?php
+			require_once 'connexion.php';
+			$req="select * from commande_client";
+			$exe=mysql_query($req);
+			echo "<table class='span8'>
+			<tr>
+			<th align='center'>Clients</th>
+			<th align='center'>Dates Commandes</th>
+			<th align='center'>Dates Livraisons</th>
+			<th align='center'>Montant Commandes</th>
+			<th align='center' colspan='2'>Faire</th>
+			</tr>";
+			while ($l=mysql_fetch_array($exe)) {
+				$nom=mysql_query("select nom,pnom from client where idcli=$l[0]");
+				$montant=msql_query("select SUM(mont) from commande_client a,ligne_commande b where a.idcom=b.idcom");
+				if($nom){
+					echo"<tr>
+						<td align='center'><a href=\"ligne_commande.php?id=".$l[0]."\">".$nom."</td></a>
+						<td align='center'>".$l[1]."</td>
+						<td align='center'>".$l[2]."</td>
+						<td align='center'>".$montant."</td>";
+				}
+				else{
+					mysql_error();
+				}
+			}
+			echo"</table>";
+		?>
+	</div>
+	
 
 <?php include('footer.php') ?> 
