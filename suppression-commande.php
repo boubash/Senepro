f1b761122f0019b49df97e3e7908db94b42d5545
booba <boubash4@gmail.com>
2013-10-03 18:41:47 +0000

    changement code de suppression php

---
+++ suppression-commande.php
@@ -0,0 +1,23 @@
+
+<?php
+    include("conexion.php");
+ 
+  $id  = $_GET['matricule'] ;
+  
+  $sql = "DELETE FROM commande WHERE id_commande = ".$id ;
+    
+
+  $exe = mysql_query($sql) ;
+ 
+
+  if($exe)
+  {
+   
+header("Location:commande.php");
+  }
+  else
+  {
+    echo("La suppression a echoue") ;
+    echo mysql_error();
+  }
+?>
